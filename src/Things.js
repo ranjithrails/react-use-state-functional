e06b4ac67079f0ddd58a753712eb9327124f7ec6
Ranjith <ranjith.durai89@gmail.com>
2021-11-27 16:44:53 +0530

    Updated with state complex data

---
+++ src/Things.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+
+const Things = () => {
+  const [items, setItems] = useState([]);
+  const [itemName, setItemName] = useState("");
+
+  const addItem = event => {
+    event.preventDefault();
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        name: itemName
+      }
+    ]);
+    setItemName("");
+  };
+
+return (
+  <>
+  {itemName}
+        <form onSubmit={addItem}>
+        <label>
+          <input
+            name="item"
+            type="text"
+            value={itemName}
+            onChange={e => setItemName(e.target.value)}
+          />
+        </label>
+      </form>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>  
+  </>
+)
+};
+
+export default Things;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Bulb from "./Bulb";
-
+import Things from "./Things";
+import LoginForm from "./Loginform";
 import "./styles.css";
 
 // function Bulbs() {
@@ -13,6 +14,8 @@ function App() {
   return (
     <div className="App">
       <Bulb />
+      <Things />
+      <LoginForm />
     </div>
   );
 }
